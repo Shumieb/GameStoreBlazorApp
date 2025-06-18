8567d2fe24259a0d9b84c98194882702e1ae89c9
Shumieb <shumiebc@gmail.com>
2025-06-18 23:01:28 +0100

    added functionality to edit a game

--- Gamestore.Frontend/Models/GameDetails.cs
+++ Gamestore.Frontend/Models/GameDetails.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Gamestore.Frontend.Models;
 
 public class GameDetails
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "The Name is required.")] 
+    [StringLength(50)]  
     public required string Name { get; set; }
+
+    [Required(ErrorMessage = "The Genre is required.")] 
     public string? GenreId { get; set; }
+
+    [Range(1, 100)]
     public decimal Price { get; set; }
     public DateOnly ReleaseDate { get; set; }
 }
